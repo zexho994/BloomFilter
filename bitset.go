a816b5407258ddfe4d38a4cd661c5bc1c0aa73b9
zouzhihao-994 <zouzhihao994@gmail.com>
2021-05-09 00:12:56 +0800

    BitSet, add a constructor From()

--- bitset.go
+++ bitset.go
@@ -1,1 +1,11 @@
 package BloomFilter
+
+type BitSet struct {
+	data   []uint64
+	length uint
+}
+
+// From is a constructor used to create a BitSet from an array of integers
+func From(buf []uint64) *BitSet {
+	return &BitSet{data: buf, length: uint(len(buf)) * 64}
+}
