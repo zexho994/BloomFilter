0b0f9d748551f0ae686c75bfa85244f1afc2f87f
zouzhihao-994 <zouzhihao994@gmail.com>
2021-05-09 19:44:54 +0800

    add set() & get() util test case

--- bitset_test.go
+++ bitset_test.go
@@ -20,3 +20,23 @@ func TestWordSize(t *testing.T) {
 	s := uint(64)
 	fmt.Print(s)
 }
+
+func TestBitSet_Set(t *testing.T) {
+	bset := New(640)
+	bset.Set(64)
+	bset.Set(131)
+	//bset.Set(105)
+	//bset.Set(132)
+	//bset.Set(53)
+	//bset.Set(159)
+
+	b1 := bset.Get(64)
+	b2 := bset.Get(131)
+	b3 := bset.Get(132)
+	b4 := bset.Get(63)
+
+	isTure(b1)
+	isTure(b2)
+	isTure(!b3)
+	isTure(!b4)
+}
