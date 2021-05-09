2de901cbd816da49402f74afde79bd086c976bee
zouzhihao-994 <zouzhihao994@gmail.com>
2021-05-09 19:44:40 +0800

    add set() & get()

--- bitset.go
+++ bitset.go
@@ -2,39 +2,45 @@ package BloomFilter
 
 // the wordSize of a bit set
 const wordSize = uint(64)
+const mask = wordSize - 1
 
-// log2WordSize is lg(wordSize)
+// log2WordSize is log(wordSize) => log(64) = 6
 const log2WordSize = uint(6)
 
+// MaxCapacity Cap returns the total possible capacity, or number of bits
+const MaxCapacity = ^uint(0)
+
 type BitSet struct {
-	data   []uint64
+	set    []uint64
 	length uint
 }
 
 // From is a constructor used to create a BitSet from an array of integers
 func From(buf []uint64) *BitSet {
-	return &BitSet{data: buf, length: uint(len(buf)) * 64}
+	return &BitSet{set: buf, length: uint(len(buf)) * 64}
 }
 
 func New(l uint) (bset *BitSet) {
 	defer func() {
 		if r := recover(); r != nil {
-			bset = &BitSet{length: 0, data: make([]uint64, 0)}
+			bset = &BitSet{length: 0, set: make([]uint64, 0)}
 		}
 	}()
-	bset = &BitSet{data: make([]uint64, l/64), length: l}
+	bset = &BitSet{set: make([]uint64, l/64), length: l}
 	return bset
 }
 
-// wordsNeeded calculates the number of words needed for i bits
-func wordsNeeded(i uint) int {
-	if i > (Cap() - wordSize + 1) {
-		return int(Cap() >> log2WordSize)
+// Set bit num to 1
+// "num >> log2WordSize" is to find the Set index from Set
+// "1 << (num & (wordSize - 1 ))" is to find the index from uint(64)
+func (bs *BitSet) Set(num uint) {
+	setIdx := num >> log2WordSize
+	if num > MaxCapacity || setIdx >= bs.length {
+		panic("You are exceeding the capacity")
 	}
-	return int((i + (wordSize - 1)) >> log2WordSize)
+	bs.set[setIdx] |= 1 << (num & mask)
 }
 
-// Cap returns the total possible capacity, or number of bits
-func Cap() uint {
-	return ^uint(0)
+func (bs *BitSet) Get(num uint) bool {
+	return bs.set[num>>log2WordSize]&(1<<(num&mask)) > 0
 }
